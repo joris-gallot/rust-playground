10e578c797a9632d910cdb3010fa247e48974e06
Joris Gallot <joris.gallot@365talents.com>
2023-01-21 08:09:01 +0100

    guess number: allow to set the range as input

--- src/guess_number.rs
+++ src/guess_number.rs
@@ -1,10 +1,21 @@
 use rand::Rng;
+use std::env;
 use std::io;
 
 fn main() {
-    println!("Guess the number between 1 and 10!");
+    let args: Vec<String> = env::args().collect();
+    let mut min = 1;
+    let mut max = 10;
+    let error_int_input = "Input not an integer";
 
-    let secret_number = rand::thread_rng().gen_range(1..=10);
+    if args.len() == 3 {
+        min = args[1].parse::<i32>().expect(error_int_input);
+        max = args[2].parse::<i32>().expect(error_int_input);
+    }
+
+    println!("Guess the number between {min} and {max}!");
+
+    let secret_number = rand::thread_rng().gen_range(min..=max);
 
     println!("Please input your guess.");
 
@@ -14,7 +25,7 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    let guess_number: i32 = guess.trim().parse().expect("Input not an integer");
+    let guess_number: i32 = guess.trim().parse().expect(error_int_input);
 
     if guess_number == secret_number {
         println!("Bravo! {secret_number}");
